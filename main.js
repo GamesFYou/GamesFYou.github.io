7be9d7fe08dd81dce29b684358dda5e8102fed12
GamesFYou <g4u3373@gmail.com>
2022-12-04 06:36:26 +0400

    jquery

--- main.js
+++ main.js
@@ -1,39 +1,4 @@
-mobiscroll.settings = {
-    theme: 'windows',
-    themeVariant: 'light'
-};
+//$("#login")
 
-$(function () {
 
-    var register = $('#demo').mobiscroll().popup({
-            display: 'center',
-            buttons: [{
-                text: 'Sign Up',
-                handler: 'set'
-            }]
-        }).mobiscroll('getInst'),
-
-        login = $('#login').mobiscroll().popup({
-            display: 'center',
-            buttons: [{
-                text: 'Sign In',
-                handler: 'set'
-            }],
-            onMarkupReady: function (event, inst) {
-                $('.md-social-btn', event.target).click(function () {
-                    inst.hide();
-                });
-            }
-        }).mobiscroll('getInst');
-
-    $('#showRegister').click(function () {
-        register.show();
-        return false;
-    });
-
-    $('#showLogin').click(function () {
-        login.show();
-        return false;
-    });
-
-});
+//$("#Register")
